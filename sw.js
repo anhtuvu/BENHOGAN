9fb259c529dd2ad98721893dea9f9c54ee43d34e
anhtuvu <mrtsaivu@gmail.com>
2026-04-27 08:33:20 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,40 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+const CACHE_NAME = 'golf-tracker-v1';
+const URLS_TO_CACHE = [
+  './',
+  './tuan1.html',
+  './tuan2.html',
+  './manifest.json',
+  './service-worker.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    }).catch(() => {
+      return caches.match(e.request);
+    })
   );
 });
